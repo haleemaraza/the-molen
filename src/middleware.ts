fe6ff0bf1572566d3ccc187807ff0249ac180571
Haleema Raza <152488926+haleemaraza@users.noreply.github.com>
2026-05-20 20:01:57 +0500

    Refactor middleware for async auth protection
    Updated middleware to use async auth protection.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,3 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-// // Create a route matcher for the protected routes
-// const protectedRoutes = createRouteMatcher([
-//   '/',
-//   '/studio',
-//   '/female',
-//   '/male',
-//   '/products(.*)'
-// ]);
-
-// // Define the middleware
-// const middleware = clerkMiddleware((auth, req) => {
-//   if (protectedRoutes(req)) {
-//     auth().protect();
-//   }
-// });
-
-// export default middleware;
-
-// // Configure the middleware to match specific routes
-// export const config = {
-//   matcher: [
-//     '/((?!.*\\..*|_next).*)',
-//     '/',
-//     '/(api|trpc)(.*)',
-//     '/studio/:path*' // Ensure /studio path is included
-//   ],
-// };
-
-
-// middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
@@ -40,13 +8,12 @@ const protectedRoutes = createRouteMatcher([
   '/products(.*)'
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (protectedRoutes(req)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
-
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)','/', '/(api|trpc)(.*)'],
-}
+};
